c02247874122d9a7cbeb41c948041bc0dd9d1e2f
HalfW-dev <54013154+HalfW-dev@users.noreply.github.com>
2021-11-09 22:20:04 +0700

    Made the code more concise
    Shortened variable names and comments

--- Adjugate Matrix Calculator.cpp
+++ Adjugate Matrix Calculator.cpp
@@ -2,13 +2,13 @@
 #include <vector>
 #include <cmath>
 
-    int determinant(std::vector<std::vector<int>> matrix, int n) //Having the determinant function separated from the rest of the program
+    int det(std::vector<std::vector<int>> mat, int n) //Determinant function
     {
-        int det = 0;
-        std::vector<std::vector<int>> submatrix(n, std::vector<int>(n, 0)); //Blank matrix so I don't have to deal with the push shenanigans
+        int d = 0;
+        std::vector<std::vector<int>> submat(n, std::vector<int>(n, 0)); //Blank matrix so no push shenanigans
 
-        if (n == 1) return (matrix[0][0]);
-        if (n == 2) return ((matrix[0][0] * matrix[1][1]) - (matrix[1][0] * matrix[0][1]));
+        if (n == 1) return (mat[0][0]);
+        if (n == 2) return ((mat[0][0] * mat[1][1]) - (mat[1][0] * mat[0][1]));
 
         else {
             for (int x = 0; x < n; x++)
@@ -21,58 +21,57 @@
                     {
                         if (j == x)
                             continue;
-                        submatrix[subi][subj] = matrix[i][j];
+                        submat[subi][subj] = mat[i][j];
                         subj++;
                     }
                     subi++;
                 }
-                det = det + (pow(-1, x) * matrix[0][x] * determinant(submatrix, n - 1));
+                d = d + (pow(-1, x) * mat[0][x] * det(submat, n - 1));
             }
         }
-        return det;
+        return d;
     }
 
-    int minor_matrix_det_calculator(std::vector<std::vector<int>> matrix, int i, int j, int n) //Specifying the ij element for Laplace expansion for all elements in the argument matrix
+    int minor_matrix_det_calculator(std::vector<std::vector<int>> mat, int i, int j, int n) //Specifies the ij element for Laplace expansion for all elements in the argument matrix
     {
-        std::vector<std::vector<int>> minor(n-1, std::vector<int>(n-1, 0));; //Placeholder for every minor matrix calculated, also blank matrix so I don't have to deal with the push shenanigans
+        std::vector<std::vector<int>> minor(n-1, std::vector<int>(n-1, 0));; //Placeholder for minor matrix , also blank mat so no shenanigans
         int i_minor = 0;
         int j_minor = 0;
 
-        for (int a = 0; a < n; a++) //Looping through the rows of the argument matrix
+        for (int a = 0; a < n; a++) //Loops through row of matrix
         {
-            if (a == i) continue; //Opting out all elements of the i-th row
+            if (a == i) continue; //Opts out all elements of the i-th row
             else
             {
-                for (int b = 0; b < n; b++) //Looping through the columns of the argument matrix
-                    if (b == j) continue; // Opting out all elements of the n-th column
+                for (int b = 0; b < n; b++) //Loops through the column of matrix
+                    if (b == j) continue; // Opts out all elements of the n-th column
                     else
                     {
-                        if (j_minor >= n - 1) //Moving on to the next row at the end of row for the minor matrix
+                        if (j_minor >= n - 1) //Moves to next row at the end of row 
                         {
                             j_minor = 0;
                             i_minor = i_minor + 1;
                         }
 
-                        minor[i_minor][j_minor] = matrix[a][b];
+                        minor[i_minor][j_minor] = mat[a][b];
                         j_minor = j_minor + 1;
-                        minor;
                     }
             }
         }
 
-        int det_minor = determinant(minor, n - 1);
+        int det_minor = det(minor, n - 1);
         return det_minor;
     }
 
-    void cofactor_matrix_and_determinants(std::vector<std::vector<int>> matrix, int n)
+    void cofactor_matrix_and_determinants(std::vector<std::vector<int>> mat, int n)
     {
-        std::vector<std::vector<int>> cofactor_matrix(n, std::vector<int>(n, 0));//Blank matrix so I don't have to deal with the push shenanigans
+        std::vector<std::vector<int>> cft_mat(n, std::vector<int>(n, 0));//Blank mat so I don't have to deal with the push shenanigans
         for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < n; j++)
             {
-                if ((i + j) % 2 == 0) cofactor_matrix[i][j] = minor_matrix_det_calculator(matrix, i, j, n);
-                else cofactor_matrix[i][j] = -minor_matrix_det_calculator(matrix, i, j, n);
+                if ((i + j) % 2 == 0) cft_mat[i][j] = minor_matrix_det_calculator(mat, i, j, n);
+                else cft_mat[i][j] = -minor_matrix_det_calculator(mat, i, j, n);
             }
         }
 
@@ -81,12 +80,12 @@
         for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < n; j++)
-                std::cout << cofactor_matrix[i][j] << " ";
+                std::cout << cft_mat[i][j] << " ";
             std::cout << std::endl;
         }
 
-        std::cout << "Determinant of the matrix is: " << determinant(matrix, n) << std::endl;
-        std::cout << "Determinant of the cofactor matrix is: " << determinant(cofactor_matrix, n);
+        std::cout << "Determinant of the matrix is: " << det(mat, n) << std::endl;
+        std::cout << "Determinant of the cofactor matrix is: " << det(cft_mat, n);
     }
 
     int main()
@@ -96,15 +95,15 @@
         std::cout << "Enter dimension of square matrix (n x n): ";
         std::cin >> n;
 
-        std::vector<std::vector<int>> matrix(n, std::vector<int>(n, 0));//Blank matrix so I don't have to deal with the push shenanigans
+        std::vector<std::vector<int>> mat(n, std::vector<int>(n, 0));//Blank mat so I don't have to deal with the push shenanigans
 
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
             {
-                std::cin >> matrix[i][j];
+                std::cin >> mat[i][j];
             }
 
-       cofactor_matrix_and_determinants(matrix, n);
+       cofactor_matrix_and_determinants(mat, n);
        return 0;
     }
 
@@ -117,4 +116,4 @@
 //        1 2 3 (press Enter)
 //        4 5 6 (press Enter)
 //        7 8 9 (press Enter)
-//3. Output will include the cofactor matrix of the provided matrix, and the determinant of both the provided matrix and the cofactor matrix.
+//3. Output will include the cofactor mat of the provided matrix, and the determinant of both the provided matrix and the cofactor matrix.
